a1316c83e60a570e170e5554b4a3f5c10931387c
Andrey Orlov <a.orlov@group-ib.com>
2026-02-12 01:00:02 +0500

    fix error with <class 'django.urls.exceptions.NoReverseMatch'>

--- netbox_cisco_psirt/urls.py
+++ netbox_cisco_psirt/urls.py
@@ -5,11 +5,17 @@ from . import models, views
 urlpatterns = [
     # Advisories
     path('advisories/', views.AdvisoryListView.as_view(), name='advisory_list'),
+    path('advisories/add/', views.AdvisoryEditView.as_view(), name='advisory_add'),
     path('advisories/<int:pk>/', views.AdvisoryView.as_view(), name='advisory'),
+    path('advisories/<int:pk>/edit/', views.AdvisoryEditView.as_view(), name='advisory_edit'),
+    path('advisories/<int:pk>/delete/', views.AdvisoryDeleteView.as_view(), name='advisory_delete'),
     path('advisories/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='advisory_changelog', kwargs={'model': models.Advisory}),
 
     # Vulnerabilities
     path('vulnerabilities/', views.VulnerabilityListView.as_view(), name='vulnerability_list'),
+    path('vulnerabilities/add/', views.VulnerabilityEditView.as_view(), name='vulnerability_add'),
     path('vulnerabilities/<int:pk>/', views.VulnerabilityView.as_view(), name='vulnerability'),
+    path('vulnerabilities/<int:pk>/edit/', views.VulnerabilityEditView.as_view(), name='vulnerability_edit'),
+    path('vulnerabilities/<int:pk>/delete/', views.VulnerabilityDeleteView.as_view(), name='vulnerability_delete'),
     path('vulnerabilities/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='vulnerability_changelog', kwargs={'model': models.Vulnerability}),
 ]
--- netbox_cisco_psirt/views.py
+++ netbox_cisco_psirt/views.py
@@ -8,9 +8,21 @@ class AdvisoryListView(generic.ObjectListView):
 class AdvisoryView(generic.ObjectView):
     queryset = models.Advisory.objects.all()
 
+class AdvisoryEditView(generic.ObjectEditView):
+    queryset = models.Advisory.objects.all()
+
+class AdvisoryDeleteView(generic.ObjectDeleteView):
+    queryset = models.Advisory.objects.all()
+
 class VulnerabilityListView(generic.ObjectListView):
     queryset = models.Vulnerability.objects.all()
     table = tables.VulnerabilityTable
 
 class VulnerabilityView(generic.ObjectView):
+    queryset = models.Vulnerability.objects.all()
+
+class VulnerabilityEditView(generic.ObjectEditView):
+    queryset = models.Vulnerability.objects.all()
+
+class VulnerabilityDeleteView(generic.ObjectDeleteView):
     queryset = models.Vulnerability.objects.all()
